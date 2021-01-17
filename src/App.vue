537c2acee3931c30deb9dbd5a1c294781caf8e3f
Ola M <Ola10417@Gmail.com>
2021-01-17 23:20:20 +0100

    added quiz component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>QUIZ Z JĘZYKA ANGIELSKIEGO</h1>
+    <button @click="startQuiz" v-if="!isQuizStarted">Zacznij quiz</button>
+    <Quiz v-if="isQuizStarted" :questionsAndAnswers=questionsAndAnswers :indexQuestion=indexQuestion @nextQuestion="nextQ"/>
+   
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Quiz from './components/Quiz.vue';
 export default {
+  components:{
+    Quiz,
+  },
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      isQuizStarted:false,
+      indexQuestion:0,
+      questionsAndAnswers:[
+        {id:1, question:'... sofa do you like better; the black or the white one?', answerA:'Which', answerB:'What', answerC:'Who'},
+        {id:2, question:'Are you enjoying ...?', answerA:'your', answerB:'yourselves', answerC:'you'},
+        {id:3, question:'What would you like to have for ... lunch today?', answerA:'a', answerB:'the', answerC:'-'},
+        {id:4, question:'Let ... do whatever I want.', answerA:'I', answerB:'me', answerC:'my'},
+        {id:5, question:'I ... France twice in my life.', answerA:'have been in', answerB:'have been to', answerC:'went to'},
+        {id:6, question:'If the police had not caught him, he ... in prison now.', answerA:'will not be', answerB:'would not have been', answerC:'would not be'},
+      ],
+      correctAnswers:['Which', 'yourselves', '-', 'me', 'have been to','would not be',]
+    }
+  },
+  methods:{
+    startQuiz()
+    {
+      this.isQuizStarted=true
+    },
+    nextQ(){
+      if(this.indexQuestion<5)
+      {
+        this.indexQuestion++
+      }
+      if(this.indexQuestion==5)
+      {
+        this.isQuizStarted=false
+        this.indexQuestion=0
+      }
+      
+    }
   }
 }
 </script>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <h2>{{questionsAndAnswers[indexQuestion].question}}</h2>
+    <button>{{questionsAndAnswers[indexQuestion].answerA}}</button>
+    <button>{{questionsAndAnswers[indexQuestion].answerB}}</button>
+    <button>{{questionsAndAnswers[indexQuestion].answerC}}</button>
+    <br>
+    <button @click="nextQuestion">Dalej</button>
+    
+    <h3>Ilość poprawnych odpowiedzi: 0/6</h3>
+    </div>
+</template>
+<script>
+export default {
+    props: [
+        'questionsAndAnswers',
+        'indexQuestion'
+    ],
+    methods: {
+        nextQuestion(){
+            this.$emit('nextQuestion');
+        }
+    }
+}
+</script>
